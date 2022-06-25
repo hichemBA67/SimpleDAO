b98fcd0bf9235a406c2b4205f1f8e4121a5ba2e2
Hichem Ben Aoun <80069620+hichemBA67@users.noreply.github.com>
2022-06-25 11:09:16 +0200

    Minor changes

--- truffle-config.js
+++ truffle-config.js
@@ -3,17 +3,17 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*", // Match any network id
+    },
   },
 
   compilers: {
     solc: {
-      version: '0.8.9',
+      version: "0.8.9",
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+        runs: 200,
+      },
+    },
+  },
+};
